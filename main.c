96ad8a117a0fb40e080a42920980b58a4d919055
Jeongwon-Ryu <113072586+Jeongwon-Ryu@users.noreply.github.com>
2022-09-13 16:58:26 +0900

    initial again

--- main.c
+++ main.c
@@ -4,6 +4,6 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-    print("Hello, Sookmyung Electronics!/n");
+    printf("%d+%d=%d/n, a, b, a+b");
 	return 0;
 }
